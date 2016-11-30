11b6ea5cfcd9da0fdc1b33063109f6b90a1b0dd0
cgonzalezsanc <carlosgosa94@gmail.com>
2016-11-30 16:56:39 +0100

    Añadido six-pack y que se ejecute con otras bebidas

--- Bottles.py
+++ Bottles.py
@@ -1,31 +1,35 @@
 class Bottles:
 
-    def song(self):
-        return self.verses(99,0)
+	def song(self, beverage = 'beer'):
+		return self.verses(99,0, beverage)
 
-    def verses(self, last, first):
-        o = self.verse(last)
-        for i in range (last-1, first-1, -1):
-            o += "\n" + self.verse (i) 
-        return o
+	def verses(self, last, first, beverage = 'beer'):
+		o = self.verse(last, beverage)
+		for i in range (last-1, first-1, -1):
+			o += "\n" + self.verse (i,beverage) 
+		return o
 
-    def verse(self, number = None):
-        if number == 0:
-            return """No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"""
-        elif number == 1:
-            return """1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-"""
-        elif number == 2:
-            return """2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-"""
-        else:
-           return """{0} bottles of beer on the wall, {0} bottles of beer.
-Take one down and pass it around, {1} bottles of beer on the wall.
-""".format(number, number-1)
+	def verse(self, number = None, beverage = 'beer'):
+		if number == 0:
+			return """No more bottles of {0} on the wall, no more bottles of {0}.
+Go to the store and buy some more, 99 bottles of {0} on the wall.
+""".format(beverage)
+		elif number == 1:
+			return """1 bottle of {0} on the wall, 1 bottle of {0}.
+Take it down and pass it around, no more bottles of {0} on the wall.
+""".format(beverage)
+		elif number == 2:
+			return """2 bottles of {0} on the wall, 2 bottles of {0}.
+Take one down and pass it around, 1 bottle of {0} on the wall.
+""".format(beverage)
+		elif number == 6:
+			return """1 six-pack of {0} on the wall, 1 six-pack of {0}.
+Take one down and pass it around, no more six-pack of {0} on the wall.
+""".format(beverage)
+		else:
+			return """{0} bottles of {2} on the wall, {0} bottles of {2}.
+Take one down and pass it around, {1} bottles of {2} on the wall.
+""".format(number, number-1, beverage)
 
 
 
